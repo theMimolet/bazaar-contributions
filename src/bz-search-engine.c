033d4719a6cd10095e2c43d7866dac5580ce182f
Eva M <kolunmi@posteo.net>
2026-02-22 19:03:40 -0700

    SearchEngine: increase score multiplier for boosted results

--- src/bz-search-engine.c
+++ src/bz-search-engine.c
@@ -570,7 +570,7 @@ query_sub_task_fiber (QuerySubTaskData *data)
             continue;
 
           if (g_hash_table_contains (bias->boost, id))
-            score *= 2.0;
+            score *= 4.0;
         }
 
       if (score > threshold)
